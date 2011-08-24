a01c52de6bc259bd43bc45a5e817f3f5e96eb664
bnc <biswanath.chowdhury@sage-mgt.net>
2011-08-24 15:59:54 -0400

    Change to read sampler db.

--- readDb.py
+++ readDb.py
@@ -1,11 +1,11 @@
 import sqlite3
 import utilDb
 
-trlConn = sqlite3.connect('Trial11_conc.db')
+trlConn = sqlite3.connect('t05.smp.db')
 trlConn.row_factory = sqlite3.Row
 trlCur = trlConn.cursor()
 
-ySmp = utilDb.db2Array(trlCur,'SELECT xd from probdose group by dNo order by dNo',dim=1)
+ySmp = utilDb.db2Array(trlCur,'SELECT smpID from samTable',dim=1)
 print ySmp
 
 trlConn.close()
