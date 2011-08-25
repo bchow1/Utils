4898f3af777e3afa079b9f18b54bfe007d64a697
bnc <biswanath.chowdhury@sage-mgt.net>
2011-08-25 18:07:32 -0400

    Add option to pass col types and separator character

--- tab2db.py
+++ tab2db.py
@@ -10,32 +10,47 @@ Convert data table from ASCII to db. Use the column names
 from first line which may or may not start with '#'.
 Ignores all blank lines.
 '''
-def makeDb(fName):
-  colTypes = None
+def makeDb(fName,separator=None,colTypes=None):
+  initDb = True
   for line in fileinput.input(fName):
     if fileinput.lineno() == 1:
-      colNames = line.replace('#','').strip().split()
-      #print 'Column names = ',colNames
+      if separator is None:
+        colNames = line.replace('#','').strip().split()
+      else:
+        colNames = line.replace('#','').strip().replace('"','').split(separator)
+        for i,colName in enumerate(colNames):
+         colNames[i] = colName.strip().replace(' ','')
+         if colName.startswith('_'):
+           colNames[i] = colName[1:] 
+      print 'Column names = ',colNames
+      if colTypes is not None:
+        if len(colTypes) != len(colNames):
+          print 'Error: Column types does not match number of column names = ',colTypes
+          sys.exit()
     else:
-      line = line.split('#')[0]
-      if len(line.strip()) > 0 :
-        colValues = line.split()
-        if not colTypes:
-          colTypes = []
-          for colValue in colValues: 
-            try:
-              colValue = ast.literal_eval(colValue)
-              if isinstance(colValue,int):
-                colTypes.append('integer')
-              elif isinstance(colValue,float):
-                colTypes.append('real')
-              else:
-                print 'Error: unknown type for ',colValue
-                sys.exit()
-            except ValueError:
-              colTypes.append('string')
-          #print 'colValues = ',colValues
-          #print 'colTypes = ',colTypes
+      line = line.split('#')[0].strip().replace('"','')
+      if len(line) > 0 :
+        if separator is None:
+          colValues = line.split()
+        else:
+          colValues = line.split(separator)
+        if initDb:
+          print 'colValues = ',colValues
+          if not colTypes:
+            colTypes = []
+            for colValue in colValues: 
+              try:
+                colValue = ast.literal_eval(colValue)
+                if isinstance(colValue,int):
+                  colTypes.append('integer')
+                elif isinstance(colValue,float):
+                  colTypes.append('real')
+                #else:
+                #  print 'Error: unknown type for ',colValue
+                #  sys.exit()
+              except ValueError:
+                colTypes.append('string')
+          # end if not colTypes
           dbFile = fName + '.db'
           dbConn = sqlite3.connect(dbFile)
           dbCur = dbConn.cursor() 
@@ -47,17 +62,23 @@ def makeDb(fName):
               createStr += ')'
             else:
               createStr += ', '
-          print createStr
+          print '\n',createStr
           dbCur.execute(createStr)
-        # end if not colTypes
+          initDb = False
+        # end if initDb
         insertStr = 'INSERT into dataTable VALUES('
         for i in range(len(colNames)):
-          insertStr += colValues[i]
+          if colTypes[i] == 'string':
+            insertStr += "'" + colValues[i] + "'"
+          else:
+            if len(colValues[i]) < 1:
+              colValues[i] = '-9999'
+            insertStr += colValues[i]
           if i == len(colNames)-1:
             insertStr += ')'
           else:
             insertStr += ', '
-        #print insertStr
+        print insertStr
         dbCur.execute(insertStr)
       else:   # skip blank lines
        continue
@@ -70,15 +91,37 @@ def makeDb(fName):
   return
 
 if __name__ == '__main__':
+
+  import optparse
+
   # local modules
   import utilDb
+
   if sys.argv.__len__() < 2:
     print 'Usage: tab2db.py table1.txt [ table2.txt ... ]'
     sys.exit()
-  for i in range(1, sys.argv.__len__()):
-    print 'Running makeDb for file ',i,':',sys.argv[i]
-    fName = sys.argv[i]
-    makeDb(fName)
+  arg = optparse.OptionParser()
+  arg.add_option("-s",action="store",type="string",dest="separator")
+  arg.add_option("-t",action="store",type="string",dest="coltype")
+  arg.set_defaults(separator=None)
+  arg.set_defaults(coltype=None)
+  opt,args = arg.parse_args()
+  print 'Using Separator = ',opt.separator
+  if opt.coltype is None:
+    colTypes = None
+  else:
+    colTypes = []
+    for cType in list(opt.coltype.strip()):
+      if cType == 'i':
+        colTypes.append('integer')
+      elif cType == 'r':
+        colTypes.append('real')
+      else:
+        colTypes.append('string')
+  print 'Using Column types = ',colTypes
+  for fName in args:
+    print '\nRunning makeDb for file ',fName
+    makeDb(fName,separator=opt.separator,colTypes=colTypes)
     dbFile = fName + '.db'
-    dataArray = utilDb.db2Array(dbFile,'select * from dataTable')
-    print dataArray
+    #dataArray = utilDb.db2Array(dbFile,'.schema')
+    #print dataArray
