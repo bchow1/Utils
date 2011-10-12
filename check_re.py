e61b60c347477abc433d8f4fdb09a2f22c822319
bnc <biswanath.chowdhury@sage-mgt.net>
2011-10-12 16:41:42 -0400

    Tested code to get nzb and zb

--- check_re.py
+++ check_re.py
@@ -4,19 +4,23 @@ import sys
 import re
 import fileinput
 
-inFile = 'Fwd048_Predict.inp'
+inFile = 'OLAD_S251.msc'
+#inFile = 'Fwd048_Predict.inp'
 #inFile = 'Fwd048_Actual.inp'
 #inFile = '../../Fwdxxx.inp'
-inpNml = ['matdef','options']
+#inpNml = ['matdef','options']
+inpNml = ['options']
 endNml = '/'
 for nml in inpNml:
   #Cpattern = '\s*&%s\s*'%nml
   #pattKey  = re.compile(Cpattern,re.I)
-  pattKey  = re.compile("(.*SMPFILE\s*=\s*)(.*?)(\s*,|$.*)",re.I)
+  #pattKey  = re.compile("(.*SMPFILE\s*=\s*)(.*?)(\s*,|$.*)",re.I)
   #pattKey = re.compile("(HPAC)|(SCIPUFF)\s*",re.I)
   #pattKey = re.compile("(.*@)(\d{3}.+)(\s*.*)")
+  pattKey  = re.compile(r".*\b(zb\s*=\s*)(.*?)($)",re.I)
+  
   for line in fileinput.input(inFile):
-    print 'line: ',line
+    print 'line: ',line.strip()
     matchKey = pattKey.match(line)
     if matchKey:
       print 'FOUND MATCH'
