059f07a01666ebfb4c8dff0db38ffde33da1efa9
bnc <biswanath.chowdhury@sage-mgt.net>
2012-09-05 13:42:48 +0100

    Use list of dist

--- pltCmpTva.py
+++ pltCmpTva.py
@@ -11,15 +11,12 @@ import sqlite3
 sys.path.append('C:\\cygwin\\home\\sid\\python')
 import utilDb
 
-# Code for SCIChem 2012 plots
+# Code for SCICHEM 2012 plots
 
-def mainProg(obsDbName,preDbName1,preDbName2):
+def mainProg():
 
-
-  # Observed data
-  obsConn = sqlite3.connect(obsDbName)
-  obsConn.row_factory = sqlite3.Row
-  obsCur = obsConn.cursor()
+  preDbName1 = 'tva_990715.smp.db'
+  preDbName2 = 'SCICHEM-01\\071599_vo3_lin_intel.smp.db'
 
   # Predicted data
   preConn1 = sqlite3.connect(preDbName1)
@@ -27,57 +24,87 @@ def mainProg(obsDbName,preDbName1,preDbName2):
   preCur1 = preConn1.cursor()
 
   # Predicted data
-  #preConn2 = sqlite3.connect(preDbName2)
-  #preConn2.row_factory = sqlite3.Row
-  #preCur2 = preConn2.cursor()
+  preConn2 = sqlite3.connect(preDbName2)
+  preConn2.row_factory = sqlite3.Row
+  preCur2 = preConn2.cursor()
   
-  plotConcAll(obsCur,preCur1)
+  varNames = ["SO2", "O3", "NO",  "NO2"]
   
-  obsConn.close()
+  distance = [62] #16, 62, 106]
+  times    = [7.25] #, 11.5, 11.5]
+  zSmp     = [582] #415, 582, 584]
+  
+  for idt,dist in enumerate(distance):
+
+    # Observed data
+    obsDbName = 'OBS\\tva_071599_' + str(dist) + 'km_obs6.csv.db'
+    obsConn = sqlite3.connect(obsDbName)
+    obsConn.row_factory = sqlite3.Row
+    obsCur = obsConn.cursor()
+
+    lArc = 2.*dist*np.pi/180.
+   
+    for varName in varNames:
+
+      print dist,' km, ',varName
+
+      # Observations
+
+      obsQry = 'select plumeKM, ' + varName + ' from dataTable'
+      obsArray = utilDb.db2Array(obsCur,obsQry)
+      if varName == 'SO2':
+        oMax = np.where(obsArray[:,1] == obsArray[:,1].max())[0][0]
+        print oMax,obsArray[oMax,0],obsArray[oMax,1]
+
+      # Predictions #1 (2012)
+
+      preQry1  = "select xSmp,Value from samTable a,smpTable p where a.colNo=p.colNo and "
+      preQry1 += "varName = '%s' and zSmp = %f and time = %3f"%(varName,zSmp[idt],times[idt])
+      #print preQry1
+      preArray1 = utilDb.db2Array(preCur1,preQry1)
+ 
+      if idt == 0 and varName == 'SO2':
+        iMax1 = np.where(preArray1[:,1] == preArray1[:,1].max())[0][0]
+        for i in range(len(preArray1)):
+          preArray1[i,0] = float(i-iMax1)*lArc + obsArray[oMax,0] 
+        dArray1 = preArray1[:,0]
+      else:
+        preArray1[:,0] = dArray1
+      #print dArray1
+      
+      preArray2 = utilDb.db2Array(preCur2,preQry1)
+      if idt == 0 and varName == 'SO2':
+        iMax2 = np.where(preArray2[:,1] == preArray2[:,1].max())[0][0]
+        for i in range(len(preArray2)):
+          preArray2[i,0] = float(i-iMax2)*lArc + obsArray[oMax,0] 
+        dArray2 = preArray2[:,0]
+      else:
+        preArray2[:,0]= dArray2
+
+      #
+      plotCompConc(zSmp, varName, obsArray, preArray1, preArray2, varName)
+
+    obsConn.close()
+    
   preConn1.close()
-
-  return
-
-def plotCompConc(zSmp, varName, obsData, preData1, title):
+  preConn2.close()
+      
+def plotCompConc(zSmp, varName, obsData, preData1, preData2, title):
   #import pdb; pdb.set_trace()
   fig = plt.figure()
   plt.clf()
   fig.hold(True)
   print obsData[:,1]
-  plt.plot(obsData[:,1],'go')
-  plt.plot(preData1[:])
+  plt.plot(obsData[:,0],obsData[:,1],'go')
+  plt.plot(preData1[:,0],preData1[:,1],'r+')
+  plt.plot(preData2[:,0],preData2[:,1],'b-')
   plt.ylabel(zSmp)
   plt.xlabel(varName)
   plt.title(title)
   fig.hold(False)
   plt.show()
-        
-def plotConcAll(obsCur, preCur1):
-  
-  varNames   = ["C004_","C002_", "C052_", "C001_"]
-  varSpecies = ["O3", "NO", "SO2", "NO2"]
-  
-  time = 11.5
-  
-  for zSmp in [415]:  #[415, 582, 584]
-    for i,varName in enumerate(varNames):
-
-      # Observations
-      obsQry = 'select plumeKM, ' + varSpecies[i] + ' from dataTable'
-      obsArray = utilDb.db2Array(obsCur,obsQry)
-
-      # Predictions #1 (2012) 
-      preQry1 = "select value from smpTable  where colNo in \
-                (select colNo from samTable  where varName = '%s' and zSmp = %f) and time = %3f"%(varName,zSmp, time)
-      preArray1 = utilDb.db2Array(preCur1,preQry1)
-
-      #
-      plotCompConc(zSmp, varName, obsArray, preArray1, varSpecies[i])
-    
+            
 # Main program
 if __name__ == '__main__':
-  obsDbName = 'tva_071599_16km_obs1.csv.db'
-  preDbName1 = 'tva_071599.smp.db'
-  preDbName2 = 'tva_071599.smp.db'
-  print "Creating plots for ",obsDbName,preDbName1,preDbName2
-  mainProg(obsDbName,preDbName1,preDbName2)
+  os.chdir('d:\\SCIPUFF\\runs\\EPRI\\Nash99')
+  mainProg()
