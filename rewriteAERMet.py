416a946dff824ef8680c7dd92c4e5838d7c3a9b6
bnc <biswanath.chowdhury@sage-mgt.net>
2012-09-17 13:17:01 -0400

    Merged with win version.

--- rewriteAERMet.py
+++ rewriteAERMet.py
@@ -16,17 +16,16 @@ import utilDb
 
 def mainProg():
   #os.chdir('D:\\SCICHEM-2012\\kinso2\\SCICHEM')
-  os.chdir('D:\\SCIPUFF\\runs\\EPRI\\aermod\\kinso2\\SCICHEM')
-  #os.chdir('v:\\scipuff\\runs\\EPRI\\wainwright')
-  #dataFile = '2009-10.SFC'
-
+  #os.chdir('D:\\SCIPUFF\\runs\\EPRI\\aermod\\kinso2\\SCICHEM')
+  os.chdir('/home/user/bnc/scipuff/runs/EPRI/wwright')
   
-  dataFile = 'kinso2.sfc'
+  dataFile = '2009-10.SFC'
+  #dataFile = 'kinso2.sfc'
   #dataFile = 'kinso2.pfl'
   colNames = ''
   colFormats = ''
 
-  if dataFile.endswith('.sfc'):
+  if dataFile.lower().endswith('.sfc'):
     #   9,   9,      1,    244    1,\
     # -40.9,  0.755,  -9.000, -9.000, -999., 1510., 927.9, 0.1500, 6.00, 1.00,\
     #  7.57    173.0    7.9     287.1    2.0     0     0.80    66.     987.    10      ADJ-A1
@@ -37,8 +36,7 @@ def mainProg():
                   'float','float','float','float','float','float','float','float','float','float',\
                   'float','float') # ,'float','float','float','S20')
 
-
-  if dataFile.endswith('.pfl'):
+  if dataFile.lower().endswith('.pfl'):
     # 80  8 30  3  100.0 1  159.    8.99    20.2    4.3    0.12
     colNames = ('year','month','day','hour', 'height',\
                  'top', 'WDnn', 'WSnn', 'TTnn', 'SAnn', 'SWnn')
@@ -68,9 +66,25 @@ def mainProg():
     sfcMasked = ma.masked_where(sfcMasked == -999.,sfcMasked)
     sfcMasked = ma.masked_where(sfcMasked == -999.9,sfcMasked)
     sfcMasked = ma.masked_where(sfcMasked == -99999.0,sfcMasked)
+    if colName.lower() == "temp":
+      sfcMasked = ma.masked_where(sfcMasked > 998.0,sfcMasked)
+    if colName.lower() == "zo":
+      sfcMasked = ma.masked_where(sfcMasked < 0.001,sfcMasked)
+      sfcMasked = ma.filled(sfcMasked,0.01)
+      
+
+    if colName.lower() == "wdnn":
+      sfcMasked = ma.masked_where(sfcMasked > 360.0,sfcMasked)
+    if colName.lower() == "wsnn":
+      sfcMasked = ma.masked_where(sfcMasked > 998.0,sfcMasked)
+    if colName.lower() == "sann":
+      sfcMasked = ma.masked_where(sfcMasked > 360.0,sfcMasked)
+    if colName.lower() == "swnn":
+      sfcMasked = ma.masked_where(sfcMasked == 99.0,sfcMasked)
     if colName == "TTnn":
       sfcMasked = ma.masked_where(sfcMasked > 99.0,sfcMasked)
       sfcMasked = ma.masked_where(sfcMasked < -10.,sfcMasked)
+
     print colName,sfcMasked.min(),sfcMasked.max()
 
     figName = colName + '.png'
@@ -85,13 +99,13 @@ def mainProg():
   # Write new data files
   
   sfcFile = open('new_' + dataFile,'w')
-  if dataFile.endswith('.sfc'):
+  if dataFile.lower().endswith('.sfc'):
       # (3(I2,1X), I3,1X, I2,1X, F6.1,1X, 2(F6.3,1X), F5.0,1X, F8.1,1X, F5.2,1X,
       # 2(F6.2,1X), F7.2,1X, F5.0, 3(1X,F6.1))
       s  = '{0[0]:2d} {0[1]:2d} {0[2]:2d} {0[3]:3d} {0[4]:2d} {0[5]:6.1f} '
       s += '{0[6]:6.3f} {0[7]:6.3f} {0[8]:5.0f} {0[9]:8.1f} {0[10]:5.2f} {0[11]:6.2f} {0[12]:6.2f} '
       s += '{0[13]:7.2f} {0[14]:6.1f} {0[15]:6.1f} {0[16]:6.1f} {0[17]:6.1f}\n'
-  if dataFile.endswith('.pfl'):
+  if dataFile.lower().endswith('.pfl'):
       # 4(I2,1X), F6.1,1X, I1,1X, F5.0,1X, F7.2,1X, F7.1, 1X,F6.1, 1X,F7.2
       s  = '{0[0]:2d} {0[1]:2d} {0[2]:2d} {0[3]:2d} {0[4]:6.1f} {0[5]:1d} {0[6]:5.0f} '
       s += '{0[7]:7.2f} {0[8]:7.1f} {0[9]:6.1f} {0[10]:7.2f}\n'
@@ -100,7 +114,6 @@ def mainProg():
         sfcFile.write(s.format(metDat[row]))
         
   sfcFile.close()
-
 
 # Main program
 if __name__ == '__main__': 
