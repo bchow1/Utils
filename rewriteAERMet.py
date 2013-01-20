a6be80b8553db336ba6f96f32120cdc51825bdfb
bnc <biswanath.chowdhury@sage-mgt.net>
2013-01-20 18:46:18 +0000

    Update the code to use variable format for writing the undefined string. Add back the temp column in the sfc file which was missing earlier

--- rewriteAERMet.py
+++ rewriteAERMet.py
@@ -14,27 +14,39 @@ def mainProg(dataFile):
   colFormats = ''
 
   if dataFile.lower().endswith('.sfc'):
-    #   9,   9,      1,    244    1,\
-    # -40.9,  0.755,  -9.000, -9.000, -999., 1510., 927.9, 0.1500, 6.00, 1.00,\
-    #  7.57    173.0    7.9     287.1    2.0     0     0.80    66.     987.    10      ADJ-A1
-    colNames = ('year','month','day','j_day','hour',\
-                'H','u','w','VPTG','Zic','Zim','L','Zo','Bo','r',\
-                'Ws','Wd','zref','temp','ztemp','col1','col2') # ,'c3','c4','c5','c6')
-    colFormats = ('int','int','int','int','int','float','float','float','float','float',\
-                  'float','float','float','float','float','float','float','float','float','float',\
-                  'float','float') # ,'float','float','float','S20')
+    
+    skiprows = 1
+    if dataFile.startswith('new_'):
+      colNames   = ('year','month','day','j_day','hour','H','u','w','VPTG','Zic',\
+                    'Zim','L','Zo','Bo','r','Ws','Wd','zref','temp','ztemp')
+      colFormats = ('int','int','int','int','int','float','float','float','float','float',\
+                    'float','float','float','float','float','float','float','float','float','float')
+    else:
+      # 9, 9, 1, 244, 1, -40.9,  0.755,  -9.000, -9.000, -999., \
+      # 1510., 927.9, 0.1500, 6.00, 1.00, 7.57, 173.0, 7.9, 287.1,\
+      # 2.0     0     0.80    66.     987.    10      ADJ-A1
+      colNames = ('year','month','day','j_day','hour','H','u','w','VPTG','Zic',\
+                  'Zim','L','Zo','Bo','r','Ws','Wd','zref','temp','ztemp',\
+                  'col1','col2') # ,'c3','c4','c5','c6')
+      colFormats = ('int','int','int','int','int','float','float','float','float','float',\
+                    'float','float','float','float','float','float','float','float','float','float',\
+                    'float','float') # ,'float','float','float','S20')
+    
+      sfcFile = open(dataFile,'r')
+      for line in sfcFile:
+        sfcHead = line
+        break
+      sfcFile.close()
 
   if dataFile.lower().endswith('.pfl'):
+    skiprows = 0
     # 80  8 30  3  100.0 1  159.    8.99    20.2    4.3    0.12
-    colNames = ('year','month','day','hour', 'height',\
+    colNames   = ('year','month','day','hour', 'height',\
                  'top', 'WDnn', 'WSnn', 'TTnn', 'SAnn', 'SWnn')
     colFormats = ('int','int','int','int','float', \
                     'int','float','float','float','float','float') 
-
-  #for colNo,colName in enumerate(colNames):
-  #  print colNo,colName
     
-  metDat = np.loadtxt(dataFile,skiprows=1,dtype={'names':colNames,'formats':colFormats})
+  metDat = np.loadtxt(dataFile,skiprows=skiprows,dtype={'names':colNames,'formats':colFormats})
   print np.shape(metDat), '\n'
 
   # Set hr from 1st observation  
@@ -59,12 +71,14 @@ def mainProg(dataFile):
     if colName.lower() == "zo":
       sfcMasked = ma.masked_where(sfcMasked < 0.001,sfcMasked)
       sfcMasked = ma.filled(sfcMasked,0.01)
-      
-
+    
+    if colName.lower() in ["w"]:
+      sfcMasked = ma.masked_where(sfcMasked < 0.02,sfcMasked)
     if colName.lower() in ["wd","wdnn","sann"]:
       sfcMasked = ma.masked_where(sfcMasked > 360.0,sfcMasked)
     if colName.lower() in ["ws","wsnn"]:
       sfcMasked = ma.masked_where(sfcMasked > 998.0,sfcMasked)
+      sfcMasked = ma.masked_where(sfcMasked < 0.2,sfcMasked)
     if colName.lower() == "swnn":
       sfcMasked = ma.masked_where(sfcMasked == 99.0,sfcMasked)
     if colName.lower() == "TTnn":
@@ -84,34 +98,43 @@ def mainProg(dataFile):
 
   # Write new data files
   
-  sfcFile = open('new_' + dataFile,'w')
-  if dataFile.lower().endswith('.sfc'):
+  if not dataFile.startswith('new_'):
+    metFile = open('new_' + dataFile,'w')
+    
+    if dataFile.lower().endswith('.sfc'):
       # (3(I2,1X), I3,1X, I2,1X, F6.1,1X, 2(F6.3,1X), F5.0,1X, F8.1,1X, F5.2,1X,
       # 2(F6.2,1X), F7.2,1X, F5.0, 3(1X,F6.1))
-      s  = '{0[0]:2d} {0[1]:2d} {0[2]:2d} {0[3]:3d} {0[4]:2d} {0[5]:6.1f} '
-      s += '{0[6]:6.3f} {0[7]:6.3f} {0[8]:5.0f} {0[9]:8.1f} {0[10]:5.2f} {0[11]:6.2f} {0[12]:6.2f} '
-      s += '{0[13]:7.2f} {0[14]:6.1f} {0[15]:6.1f} {0[16]:6.1f} {0[17]:6.1f}\n'
-  if dataFile.lower().endswith('.pfl'):
+      fmtList = ['2d','2d','2d','3d','2d','6.1f','6.3f','6.3f','5.0f','8.1f','5.2f','6.2f','6.2f',\
+                 '7.2f','6.1f','6.1f','6.1f','6.1f','6.1f','6.1f']
+      metFile.write('%s'%sfcHead)
+    if dataFile.lower().endswith('.pfl'):
       # 4(I2,1X), F6.1,1X, I1,1X, F5.0,1X, F7.2,1X, F7.1, 1X,F6.1, 1X,F7.2
-      s  = '{0[0]:2d} {0[1]:2d} {0[2]:2d} {0[3]:2d} {0[4]:6.1f} {0[5]:1d} {0[6]:5.0f} '
-      s += '{0[7]:7.2f} {0[8]:7.1f} {0[9]:6.1f} {0[10]:7.2f}\n'
-      
-  for row in range(len(metDat)):
-        sfcFile.write(s.format(metDat[row]))
-        
-  sfcFile.close()
+      fmtList = ['2d','2d','2d','2d','6.1f','1d','5.0f','7.2f','7.1f','6.1f','7.2f']
+    
+    for row in range(len(metDat)):
+      sfmt = '' 
+      for idx,fmt in enumerate(fmtList):
+        if metDat[row][idx] == -999.0:
+          sfmt = sfmt + '{0[%d]:%s} '%(idx,'6.1f')
+        else:
+          sfmt = sfmt + '{0[%d]:%s} '%(idx,fmt)
+      sfmt = sfmt + '\n'
+      metFile.write(sfmt.format(metDat[row]))
+    metFile.close()
 
 # Main program
 if __name__ == '__main__': 
 
-  runDir = 'D:\\SCIPUFF\\EPRI\\runs\\kos_090811'
+  #runDir = 'D:\\SCIPUFF\\EPRI\\runs\\kos_090811'
   #runDir = '/home/user/bnc/scipuff/runs/EPRI/wwright'
+  runDir = 'D:\\Aermod\\v12345\\runs\\martin\\SCICHEM'
     
-  dataFile = raw_input('AERMOD datafile name? ')
+  #dataFile = raw_input('AERMOD datafile name? ')
   #dataFile = '2009-10.SFC'
   #dataFile = 'kosovo11-ww-scipuff.sfc'
   #dataFile = 'kinso2.sfc'
   #dataFile = 'kinso2.pfl'
+  dataFile = 'mc.sfc'
 
   os.chdir(runDir)
   mainProg(dataFile)
