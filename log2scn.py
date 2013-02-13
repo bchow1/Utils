cd7c5410ffd246fd0a6f5408f265c80c1d1a2e90
bnc <biswanath.chowdhury@sage-mgt.net>
2013-02-13 15:55:33 +0000

    Use tStart and tEnd instead of hardcoded values. Also write out only if cmass > 0.

--- log2scn.py
+++ log2scn.py
@@ -6,7 +6,7 @@ import fileinput
 trelPatt = re.compile('\s*TREL\s*=\s*(.+),')
 relSPatt = re.compile('\s*RELSTATUS\s*=\s*(.+),')
 
-def rdLog(fName):
+def rdLog(fName,tStart,tEnd):
   scnFile = open('temp.scn','w')
   isSCN = False
   for line in fileinput.input(fName):
@@ -21,29 +21,34 @@ def rdLog(fName):
       if line.strip().startswith('/'):
         isSCN = False
         continue
+      if 'CMASS' in line:
+        cMass = float(line.strip().split('=')[1].replace(',',''))
     elif 'Update Release' in line:
-        relTime = line.split("'")[2].strip()
-        print 'Rel Time = ',relTime
-        if float(relTime) > 2408.:
-          wrtScn(scnFile,scnLines,relTime)
-        scnLines = []
-        if float(relTime) > 2490.:
-          sys.exit()
+      relTime = line.split("'")[2].strip()
+      print 'Rel Time = ',relTime
+      if float(relTime) > tStart and cMass > 0.:
+        wrtScn(scnFile,scnLines,relTime,tStart)
+      scnLines = []
+      if float(relTime) > tEnd:
+        sys.exit()
   fileinput.close()
   scnFile.close()
 
-
-def wrtScn(scnFile,scnLines,relTime):
+def wrtScn(scnFile,scnLines,relTime,tStart):
   for line in scnLines:
     tRelMatch = trelPatt.match(line)
     relSMatch = relSPatt.match(line)
     if tRelMatch is not None:
-      line = ' TREL    =  ' + str(float(relTime) - 2408.)  + ',\n'
+      line = ' TREL    =  ' + str(float(relTime) -tStart)  + ',\n'
     if relSMatch is not None:
       line = ' RELSTATUS       =           1,\n'
     scnFile.write('%s'%line) 
   return
 
 if __name__ == "__main__":
-  logFile = 'ww_1yr.log'
-  rdLog(logFile)
+  os.chdir('d:\\Aermod\\v12345\\runs\\kinsf6\\SCICHEM')
+  logFile = 'KSF6-424_80I.log'
+  tStart  = 0.
+  tEnd    = 1e+10
+  #logFile = 'ww_1yr.log'
+  rdLog(logFile,tStart,tEnd)
