7b2d9ddb2acb631bac3742f6f661cd01e771f16c
bnc <biswanath.chowdhury@sage-mgt.net>
2013-02-27 15:16:02 +0000

    Restore scatter plot examples from plotAerModDom and plotCop

--- plotAny.py
+++ plotAny.py
@@ -7,53 +7,39 @@ import fileinput
 import math
 import numpy as np
 import numpy.ma as ma
+from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+from matplotlib import colors
+import matplotlib.cm as cm
 import measure
+
+# Local libs
+import tab2db
+import utilDb
+import utilPlot
+
 
 compName = socket.gethostname()
 
-# Local modules
+# Prints available color maps in matplotlib.cm 
+maps = sorted(m for m in plt.cm.datad if not m.endswith("_r"))
+# Only non-reverse colormaps
+print maps
+# Only reversed colormaps
+#maps = sorted(m for m in plt.cm.datad if m.endswith("_r"))
+#print maps
+#sys.exit()
+
+'''
+# Example for scatter plot showing source and receptor locations
+# read from AERMOD input file
+
 if  compName == 'pj-linux4':
-  sys.path.append('/home/user/bnc/python')
   runDir = ''
-if compName == 'sm-bnc':
-  runDir = 'D:\\Aermod\\v12345\\runs\\tracy\\SCICHEM'
-if compName == 'sage-d600':
-  runDir = 'd:\\TestHPAC\\Outputs\\120803.1900\\Experimental\\MDAPassive\\prairie\\T_long'
+else:
+  runDir = 'D:\\Aermod\\v12345\\runs\\kinsf6'
 os.chdir(runDir)
-
-fList = os.listdir('./')
-smpList = []
-for fName in fList:
-  if fName.endswith('.smp'):
-    smpList.append(fName)
-
-print len(smpList),smpList
-
-smpConc = []
-for ns,smp in enumerate(smpList):
-  smpFile = open(smp,'r')
-  for line in smpFile:
-    nVar = (int(line.strip())-1)/3
-    break
-  smpFile.close()
-  colList  = [i for i in range(1,nVar*3+1,3)]
-  print smp,colList
-  if nVar < 4:
-    skipRows = 3
-  else:
-    skipRows = 4
-
-  conc = np.loadtxt(smp,skiprows=skipRows,usecols=colList)
-  
-  smpConc.extend(list(np.reshape(conc,np.size(conc))))
-
-smpConc = np.array(smpConc)
-smpConc = np.sort(smpConc)[::-1]
-np.size(smpConc)
-np.save('smpConc.npy',smpConc)
-    
-sys.exit()
 
 sxy = []
 rxy = []
@@ -67,7 +53,7 @@ for line in fileinput.input('TRACAER.INP'):
 fileinput.close()
 
 sxy = np.array(sxy)
-rxy = np.array(rxy) 
+rxy = np.array(rxy)
 
 plt.clf()
 plt.hold(True)
@@ -75,18 +61,44 @@ plt.hold(True)
 hr = plt.scatter(rxy[:,0],rxy[:,1],color='red',marker='o',s=50)
 hs = plt.scatter(sxy[:,0],sxy[:,1],color='green',marker='^',s=50)
 
-#vmin = 0.
-#vmax = max(cSkewOP.max(),cStdOP.max())
-#plt.xlim([vmin,vmax])
-#plt.ylim([vmin,vmax])
-#plt.title('Comparison of Maximum Concentration')
-#plt.plot([vmin,vmax],[vmin,vmax],'k-')
-#plt.plot([vmin,vmax],[vmin*0.5,vmax*0.5],'r-')
-#plt.plot([vmin,vmax],[vmin*2,vmax*2],'r-')
+'''
+'''
+# Example of scatter plots with legend and factor of 2 lines.
+
+if compName == 'sage-d600':
+  runDir = 'D:\\SCICHEM-2012\\Cop'
+if compName == 'sm-bnc':
+  runDir = 'D:\\SCIPUFF\\EPRIx\\SCICHEM-2012\\runs\\COP'
+
+os.chdir(runDir)
+
+fileName = os.path.join(runDir,'cop.all')
+cSkewOP  = readcOP(fileName, "Skew")
+print 'cSkewOP',cSkewOP
+
+fileName = os.path.join(runDir,'SCICHEM-01','cop.all')
+cStdOP   = readcOP(fileName, "Standard")
+print 'cStdOP',cStdOP
+
+plt.clf()
+plt.hold(True)
+hskew = plt.scatter(cSkewOP[:,0],cSkewOP[:,1],color='black',marker='o',s=50)
+hstd  = plt.scatter(cStdOP[:,0],cStdOP[:,1],color='black',marker='^',s=50)
+
+vmin = 0.
+vmax = max(cSkewOP.max(),cStdOP.max())
+plt.xlim([vmin,vmax])
+plt.ylim([vmin,vmax])
+plt.title('Comparison of Maximum Concentration')
+plt.plot([vmin,vmax],[vmin,vmax],'k-')
+plt.plot([vmin,vmax],[vmin*0.5,vmax*0.5],'r-')
+plt.plot([vmin,vmax],[vmin*2,vmax*2],'r-')
 plt.xlabel('x')
 plt.ylabel('y')
-#plt.legend([hskew,hstd],['Skew Model','Standard Model'],bbox_to_anchor=(0.4,0.97))
+plt.legend([hskew,hstd],['Skew Model','Standard Model'],bbox_to_anchor=(0.4,0.97))
 plt.hold(False)
 plt.show()
-#plt.savefig('cop_ord.png')
+plt.savefig('cop_ord.png')
+'''
+
 
