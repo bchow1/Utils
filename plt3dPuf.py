66f29a630af172c728689d80530b100177527613
bnc <biswanath.chowdhury@sage-mgt.net>
2013-05-10 22:06:51 +0100

    Update to plot martin creek with terrain.

--- plt3dPuf.py
+++ plt3dPuf.py
@@ -1,100 +1,115 @@
 # -*- coding: utf-8 -*-
 """
 Uses of GLScatterPlotItem with rapidly-updating plots.
-
 """
+import os
+import sys
 import OpenGL
 OpenGL.USE_ACCELERATE = False
 
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
+import pyqtgraph as pg
 import numpy as np
+
+# local 
+import readPuf
+global hr, maxC
 
 app = QtGui.QApplication([])
 w = gl.GLViewWidget()
-w.opts['distance'] = 20
+w.opts['distance'] = 3
 w.show()
-w.setWindowTitle('pyqtgraph example: GLScatterPlotItem')
 
 g = gl.GLGridItem()
 w.addItem(g)
 
-##
-##  First example is a set of points with pxMode=False
-##  These demonstrate the ability to have points with real size down to a very small scale 
-## 
-pos = np.empty((53, 3))
-size = np.empty((53))
-color = np.empty((53, 4))
-pos[0] = (1,0,0); size[0] = 0.5;   color[0] = (1.0, 0.0, 0.0, 0.5)
-pos[1] = (0,1,0); size[1] = 0.2;   color[1] = (0.0, 0.0, 1.0, 0.5)
-pos[2] = (0,0,1); size[2] = 2./3.; color[2] = (0.0, 1.0, 0.0, 0.5)
+os.chdir('D:\\Aermod\\v12345\\runs\\martin\\SCICHEM')
+env = os.environ.copy()
+if sys.platform == 'win32':
+  compiler = "intel"
+  version = "Debug"
+  SCIPUFF_BASEDIR="D:\\SCIPUFF\\Repository\\"
+  binDir = os.path.join(SCIPUFF_BASEDIR,"workspace","EPRI","bin",compiler,"Win32",version)
+  iniFile = "D:\\SCIPUFF\\Repository\\workspace\\EPRI\\scipuff.ini"
+  env["PATH"] = "%s" % (binDir)
+  readpuf  = ["%s\\scipp.exe"%binDir,"-I:%s"%iniFile,"-R:RP"]
+  
+prjName = 'MCR_AER_TER'
 
-z = 0.5
-d = 6.0
-for i in range(3,53):
-    pos[i] = (0,0,z)
-    size[i] = 2./d
-    color[i] = (0.0, 1.0, 0.0, 0.5)
-    z *= 0.5
-    d *= 2.0
-    
-phase = 0.
-sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
-sp1.translate(5,5,0)
+def setHrDat(hr,maxC=None):
+
+  vNames,hrDat = readPuf.getHrDat(readpuf,env,prjName,hr,tail='\n')
+  
+  nPuf,nVar = np.shape(hrDat)
+  print 'Npuf = %d for hr %d'%(nPuf,hr)
+  
+  pos = np.empty((nPuf, 3))
+  size = np.empty((nPuf))
+  color = np.empty((nPuf, 4))
+  
+  iX  = vNames['X']
+  iY  = vNames['Y']
+  iZ  = vNames['Z']
+  iC  = vNames['C']
+  iSx = vNames['SXX']
+  iSy = vNames['SYY']
+  iSz = vNames['SZZ']
+  
+  maxX = hrDat[:,iX].max()
+  maxY = hrDat[:,iY].max()
+  maxZ = hrDat[:,iZ].max()
+  if maxC is None:
+    maxC = hrDat[:,iC].max()
+  
+  if maxX > 1e-10:
+    hrDat[:,iX] = hrDat[:,iX]/maxX
+  if maxY > 1e-10:
+    hrDat[:,iY] = hrDat[:,iY]/maxY
+  if maxZ > 1e-10:
+    hrDat[:,iZ] = hrDat[:,iZ]/maxZ
+  #if maxC > 1e-10:
+  #  hrDat[:,iC] = 1. + 0.1*max(-10.,np.log10(hrDat[:,iC]/maxC))
+  
+  for iPuf in range(nPuf):
+    pos[iPuf]   = (hrDat[iPuf,iX],hrDat[iPuf,iY],hrDat[iPuf,iZ])
+    size[iPuf]  = 0.05 # 1. + 0.1*max(-10.,np.log10(hrDat[iPuf,iC]/maxC))
+    #color[iPuf] = (0.0, 1.0, 0.0, 0.5) # Green
+    cScale = 1. + 0.33*max(-3.,np.log10(hrDat[iPuf,iC]/maxC))
+    color[iPuf] = pg.glColor((cScale,1.))
+
+  return pos,size,color,maxC
+
+hr = 12
+pos,size,color,maxC = setHrDat(hr)
+sp1 = gl.GLScatterPlotItem(pos=pos, size=size*.1, color=color, pxMode=False)
+#sp1.translate(5,5,0)
+ax = gl.GLAxisItem()
+ax.setSize(1,1,1)
+w.addItem(ax)
+w.setWindowTitle('Puff at %d hr'%hr)
 w.addItem(sp1)
 
-'''
-##
-##  Second example shows a volume of points with rapidly updating color
-##  and pxMode=True
-##
-
-pos = np.random.random(size=(100000,3))
-pos *= [10,-10,10]
-pos[0] = (0,0,0)
-color = np.ones((pos.shape[0], 4))
-d2 = (pos**2).sum(axis=1)**0.5
-size = np.random.random(size=pos.shape[0])*10
-sp2 = gl.GLScatterPlotItem(pos=pos, color=(1,1,1,1), size=size)
-phase = 0.
-
-w.addItem(sp2)
-
-
-##
-##  Third example shows a grid of points with rapidly updating position
-##  and pxMode = False
-##
-
-pos3 = np.zeros((100,100,3))
-pos3[:,:,:2] = np.mgrid[:100, :100].transpose(1,2,0) * [-0.1,0.1]
-pos3 = pos3.reshape(10000,3)
-d3 = (pos3**2).sum(axis=1)**0.5
-phase = 0.
-
-sp3 = gl.GLScatterPlotItem(pos=pos3, color=(1,1,1,.3), size=0.1, pxMode=False)
-
-w.addItem(sp3)
-
 def update():
+  ## update surface positions and colors
+  global hr,maxC
   
-    ## update surface positions and colors
-    global sp3, d3, pos3, phase
-    z = -np.cos(d3*2+phase)
-    pos3[:,2] = z
-    color = np.empty((len(d3),4), dtype=np.float32)
-    color[:,3] = 0.3
-    color[:,0] = np.clip(z * 3.0, 0, 1)
-    color[:,1] = np.clip(z * 1.0, 0, 1)
-    color[:,2] = np.clip(z ** 3, 0, 1)
-    sp3.setData(pos=pos3, color=color)
-    phase -= 0.1
+  hr += 1
+  if hr > 21:
+    hr   = 12
+    pos,size,color,maxC = setHrDat(hr)
+  else:
+    pos,size,color,dumC = setHrDat(hr,maxC=maxC)
+  w.setWindowTitle('Puff at %d hr'%hr)
+  #if np.remainder(hr,2) == 0:
+  #  color[:] = (0.0, 1.0, 0.0, 0.5)
+  #else:
+  #  color[:] = (1.0, 0.0, 0.0, 0.5)
+  sp1.setData(pos=pos,size=size*.1,color=color)
     
 t = QtCore.QTimer()
 t.timeout.connect(update)
-t.start(50)
-'''
+t.start(500)
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
