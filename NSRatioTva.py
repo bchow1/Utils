7fe73c7a228bd300aa0bb21409a369bc1f2d80cc
bnc <biswanath.chowdhury@sage-mgt.net>
2013-05-20 20:19:09 +0100

    Calculate NOy/SO2 ratio for TVA from npz files.

---
+++ NSRatioTva.py
@@ -0,0 +1,46 @@
+
+#!/bin/env python
+import os
+import sys
+import socket
+import numpy as np
+
+import measure
+
+compName = socket.gethostname()
+
+# Local modules
+if compName == 'sm-bnc' or compName == 'sage-d600':
+  sys.path.append('C:\\cygwin\\home\\sid\\python')
+if  compName == 'pj-linux4':
+  sys.path.append('/home/user/bnc/python')
+
+# Main program
+if __name__ == '__main__':
+
+  if compName == 'sm-bnc':
+    runDir = 'd:\\scipuff\\EPRIx\\SCICHEM-2012\\runs\\tva\\tva_990715'
+  if compName == 'pj-linux4':
+    runDir = '/home/user/bnc/scipuff/EPRI_121001/runs/tva'
+  if compName == 'sage-d600':
+    runDir = 'D:\\SCICHEM-2012' 
+
+  varNames = ['SO2','NOy']  
+
+  fList = os.listdir('.')
+  for fName in fList:
+    if fName.endswith('.npz'):
+      if 'SO2' in fName:
+        dist = fName.split('_')[0].replace('km','')
+        #SO2
+        npzfile = np.load(fName)
+        so2Max_obs = npzfile['arr_0'].max()
+        so2Max_pre = npzfile['arr_1'].max()        
+        #NOy
+        fName = fName.replace('SO2','NOy')  
+        print fName
+        npzfile = np.load(fName)
+        noyMax_obs = npzfile['arr_0'].max()
+        noyMax_pre = npzfile['arr_1'].max()
+        print dist,noyMax_obs/so2Max_obs,noyMax_pre/so2Max_pre
+          
