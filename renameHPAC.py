f79a759d34fc16865da9da4f560169eafc7f083c
bnc <biswanath.chowdhury@sage-mgt.net>
2013-08-21 20:47:28 +0100

    Use shutil for copying files as os.move not working on windows.

--- renameHPAC.py
+++ renameHPAC.py
@@ -1,24 +1,26 @@
 import os
 import sys
+import shutil
 import re
 import fileinput
-
-# local modules
-import utilDb
 
 # patterns
 hpacPatt  = re.compile('(.*)hpac(.*)',re.I)
 exts = ('.f','.f90','.F','.F90','.mak','.sh','.vfproj','.vcproj','.sln')
-#exts = ('.new')
 
 def getFnames(baseDir='.'):
   fList = []
-  for root,dirs,files in os.walk(baseDir):
+  dList = []
+  excludes = [os.path.join(baseDir,'.git'),os.path.join(baseDir,'lib'),'workspace','CVS'] # for dirs and files
+  for root,dirs,files in os.walk(baseDir,topdown=True):
+    # exclude dirs
+    dirs[:] = [d for d in dirs if os.path.join(root,d) not in excludes]
+    dList.append(os.path.join(root,d))
     for fName in files:
       if fName.endswith(exts):
         print os.path.join(root,fName)
         fList.append(os.path.join(root,fName))
-  return fList
+  return dList,fList
 
 def replaceH(string):
   newString = None
@@ -34,14 +36,17 @@ def replaceH(string):
 
 if __name__ == '__main__':
 
-
+  errList = []
   #os.chdir('D:\\hpac\\SCIPUFF\\export\\SCICHEM\\120719\\workspace\\EPRI')
   #os.chdir('D:\\hpac\\SCIPUFF\\export\\SCICHEM\\120719\\src\\sys\\windows')
-  os.chdir('D:\\SCIPUFF\\Repository\\EPA')
-  fList = getFnames()
+  os.chdir('D:\\SCIPUFF\\Repository\\EPA_Processed\\')
+  ans = raw_input('Renaming directories and files in %s.\n Continue? '%os.getcwd())
+  if ans == 'n' or ans == 'N':
+    sys.exit()
+  
+  dList,fList = getFnames()
   for fName in fList:
-    print 'rename File:',fName
-    newHName = fName+'.new'
+    newHName = fName +'.new'
     fNew = open(newHName,'w')
     for line in fileinput.input(fName):
       newLine = replaceH(line)
@@ -51,25 +56,38 @@ if __name__ == '__main__':
         fNew.write('%s'%line)
     fileinput.close()
     fNew.close()
+    print 'Rewriting %s as %s'%(fName,newHName)
     os.remove(fName)
 
-    '''
-    # Use following lines to rename .new files. Must create SCIP directories manually
-    newHName = fName[2:]
-    print newHName
-    newSName = replaceH(newHName.replace('.new',''))
-    print newSName
-    '''
     newSName = replaceH(fName)
-    if newSName is not None:
+    if newSName is not None:      
       try:
-        os.rename(newHName,newSName)
+        dName = os.path.dirname(newSName)
+        if not os.path.exists(dName):          
+          print 'Creating dir ',dName
+          os.makedirs(dName)
+        shutil.move(newHName,newSName)
+        print 'Renaming %s to %s in %s'%(newHName,newSName,os.getcwd())
       except OSError:
-        print 'Error: renaming ',newHName,' to ',newSName
-        break
+        print 'Failed renaming %s to %s in %s'%(newHName,newSName,os.getcwd())
+        errList.append('mv -v %s %s'%(newHName.replace('\\',r'/'),newSName.replace('\\',r'/')))
+        continue
     else:
       try:
-        os.rename(newHName,fName)
+        dName = os.path.dirname(fName)
+        if not os.path.exists(dName):
+          os.makedirs(dName)
+          print 'Creating dir ',dName
+        shutil.move(newHName,fName)
+        print 'Moving %s to %s in %s'%(newHName,fName,os.getcwd())
       except OSError:
-        print 'Error: renaming ',newHName,' to ',fName
-        break
+        print 'Failed moving %s to %s in %s'%(newHName,fName,os.getcwd())
+        errList.append('mv -v %s %s'%(newHName.replace('\\','/'),fName.replace('\\','/')))
+        continue
+  if len(errList) > 0:
+    reNameF = open('reName.sh','w')
+    for line in errList:
+      reNameF.write('%s\n'%line)
+    reNameF.close()
+ 
+  
