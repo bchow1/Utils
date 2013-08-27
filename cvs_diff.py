397527afcf5c4bbd19f3d9f18c693816e94d64f9
bnc <biswanath.chowdhury@sage-mgt.net>
2013-08-27 19:12:38 +0100

    Add script to write diff from CVS repository for a file

---
+++ cvs_diff.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import optparse
+
+#
+import run_cmd
+
+def printDiff(rev1,rev2):
+  cmd = ['C:/cygwin/bin/cvs.exe','diff','-r1.%d'%rev1,'-r1.%d'%rev2,'%s'%fName]
+  (Outputs,IOstat) = run_cmd.Command(env,cmd,(''),tail,errOut=False,outOut=True)
+  lines = Outputs.split('\n')
+  for line in lines:
+    print line
+    
+# Parse arguments
+arg = optparse.OptionParser()
+arg.add_option("-f",action="store",type="string",dest="fileName")
+arg.add_option("-s",action="store",type="string",dest="startRev")
+arg.add_option("-e",action="store",type="string",dest="endRev")
+arg.set_defaults(fileName=None,startRev=None,endRev=None)
+opt,args = arg.parse_args()
+# Check arguments
+if opt.fileName is None:
+  print 'Error: fileName must be specified'
+  print 'Usage: cvs_diff.py -f fileName [-s startRev] [ -e endRev]'
+else:
+  env = os.environ.copy()
+  if sys.platform == 'win32':
+    cygPath="C:\\cygwin\\bin"
+    oldPath = env["PATH"]
+    env["PATH"] = "%s;%s" % (cygPath,oldPath)
+    env["CVS_RSH"] = "ssh"
+    tail = '\r\n'
+    SSH_AGENT_PID = None
+    (Outputs,IOstat) = run_cmd.Command(env,['C:/cygwin/bin/printenv'],(''),tail,errOut=False)
+    lines = Outputs.split('\n')
+    for line in lines:
+      if line.strip().startswith('SSH_AGENT_PID'):
+        SSH_AGENT_PID = line.strip().split('=')[1].strip()
+      if line.strip().startswith('SSH_AUTH_SOCK'):
+        SSH_AUTH_SOCK = line.strip().split('=')[1].strip()
+    if SSH_AGENT_PID is None:
+      print 'Start ssh-agent before running cvs_diff.py'
+      sys.exit()
+    else:
+      env['SSH_AGENT_PID'] = SSH_AGENT_PID
+      env['SSH_AUTH_SOCK'] = SSH_AUTH_SOCK
+    print env['SSH_AGENT_PID']
+    print 'SSH_AUTH_SOCK:',SSH_AUTH_SOCK
+  else:
+    tail = '\n'
+  
+  # Get working and repository versions
+  dName    = os.path.dirname(opt.fileName)
+  fName    = os.path.basename(opt.fileName)
+  startRev = opt.startRev
+  endRev   = opt.endRev
+  os.chdir(dName)
+  
+  (Outputs,IOstat) = run_cmd.Command(env,['C:/cygwin/bin/cvs.exe','status','%s'%fName],(''),tail,errOut=False)
+  lines = Outputs.split('\n')
+  for line in lines:
+    if line.strip().startswith('Working revision'):
+      wrkRev = int(line.strip().split(':')[1].strip().split('.')[1])
+      print 'wrkRev:',wrkRev
+    if line.strip().startswith('Repository revision'):
+      repRev = int(line.strip().split(':')[1].split()[0].strip().split('.')[1])
+      print 'repRev:',repRev
+
+  if endRev is None:
+    endRev = repRev
+        
+  if startRev is None:
+    printDiff(wrkRev,repRev)
+    startRev = 1
+  elif startRev < 0:
+    startRev = endRev + startRev
+  
+  for iver in range(endRev,startRev,-1):
+    print '\nVersion: ',iver
+    printDiff(iver,iver-1)
+
+
