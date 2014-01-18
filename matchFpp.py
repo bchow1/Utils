f19c6a6d6027a483fd3c085a872991f41b13723b
bnc <biswanath.chowdhury@sage-mgt.net>
2014-01-18 17:53:05 -0500

    Add match string for not defined

--- matchFpp.py
+++ matchFpp.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 import os
 import sys
 import re
@@ -7,12 +7,21 @@ import fileinput
 if len(sys.argv) != 2 :
   print('Usage: matchFpp file')
 
+isFlush = True
+#isFlush = False
+#prntLNo = True
+prntLNo = False
+ 
+if isFlush: 
+  bChar =""
+else:
+  bChar =".+"
 
-ifStart = re.compile("!DEC\$ IF DEFINED\s*\(.+")
-ifStarn = re.compile("!DEC\$ IF \.NOT\.\s*DEFINED\s*\(.+")
-ifElif  = re.compile("!DEC\$ ELSEIF\s+.+")
-ifElse  = re.compile("!DEC\$ ELSE.*")
-ifEnd   = re.compile("!DEC\$ ENDIF.*")
+ifStart = re.compile(bChar+"!DEC\$ IF DEFINED\s*\(.+")
+ifStarn = re.compile(bChar+"!DEC\$ IF \.NOT\.\s*DEFINED\s*\(.+")
+ifElif  = re.compile(bChar+"!DEC\$ ELSEIF\s+.+")
+ifElse  = re.compile(bChar+"!DEC\$ ELSE.*")
+ifEnd   = re.compile(bChar+"!DEC\$ ENDIF.*")
 
 fName = sys.argv[1]
 #fName = 'D:\\hpac\\gitEPRI\\src\\lib\\SCIPUFFlib\\SCIPUFF\\initial.f90'
@@ -25,19 +34,29 @@ for line in fileinput.input(fName):
   matchElse  = ifElse.match(line)
   matchEnd   = ifEnd.match(line)
   #print fileinput.lineno(),':',line
+  #print matchStart
+  lNo = '%04d:'%fileinput.lineno()
   lNo = '%04d:'%fileinput.lineno()
   if matchStart or matchStarn or matchElse:
     if matchStart or matchStarn:
       nlev += 1 
       heads = spaces + '|-'
       spaces = spaces + '| '
-    sys.stdout.write('%s%s%s'%(lNo,heads,line))
+    if prntLNo:
+      sys.stdout.write('%s%s%s'%(lNo,heads,line))
+    else:
+      sys.stdout.write('%s%s'%(heads,line))
   elif matchEnd:
-    sys.stdout.write('%s%s%s'%(lNo,heads,line))
+    if prntLNo:
+      sys.stdout.write('%s%s%s'%(lNo,heads,line))
+    else:
+      sys.stdout.write('%s%s'%(heads,line))
     nlev -= 1 
     heads = heads[:-3] + '-'
     spaces = spaces[:-2]
   else:
-    sys.stdout.write('%s%s%s'%(lNo,spaces,line))
+    if prntLNo:
+      sys.stdout.write('%s%s%s'%(lNo,spaces,line))
+    else:
+      sys.stdout.write('%s%s'%(spaces,line))
   
-
