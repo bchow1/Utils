286a402f37b4ac13e9a4e9f3814fae79bebbd96f
bnc <biswanath.chowdhury@sage-mgt.net>
2014-05-13 13:51:44 -0400

    Add option to plot puff distortion.

--- cmpPuffCsv.py
+++ cmpPuffCsv.py
@@ -8,97 +8,148 @@ import matplotlib.pyplot as plt
 
 #os.chdir('d:\\SCIPUFF\\runs\\HPAC\\omp\\simple\\lomp')
 
-csvFile  = sys.argv[1]
-nameList = sys.argv[2].split(',')
+csvFiles = []
+nFiles   = sys.argv.__len__()
+if nFiles < 2:
+  print 'For Plotting: '
+  print 'Usage: cmpPuffCsv.py csvFile1 csvFile2'
+  print
+  print 'Else: '
+  print 'Usage: cmpPuffCsv.py csvFile'
+  print
+  sys.exit()
+elif nFiles == 1:
+  print
+  print 'Skipping Plotting'
+  print
+
+for i in range(1,nFiles):
+  csvFiles.append(sys.argv[i])
+
+pDat = [[] for i in range(1,nFiles)]
+#print 'csvFiles = ',csvFiles
+  
+#nameList = sys.argv[2].split(',')
+nameList = 'ipuf,x,y,z,sxx,sxy,sxz,syy,syz,szz,c'.split(',')
 
 #print 'nameList = ',nameList
 
-if not os.path.exists(csvFile):
-  print 'Error: cannot find csvFile = ',csvFile
-#print 'csvFile = ',csvFile
+for fNo,csvFile in enumerate(csvFiles):
 
-for line in fileinput.input(csvFile):
+  if not os.path.exists(csvFile):
+    print 'Error: cannot find csvFile = ',csvFile
+  print 'csvFile = ',csvFile
 
-  if fileinput.lineno() == 1:
-    title,tvalue = line.split(',')
-    npuf = int(tvalue.split('=')[-1].split('"')[0])
-    #print 'npuf = ',npuf
+  for line in fileinput.input(csvFile):
 
-  if fileinput.lineno() == 2:
-    nvar = int(line.split(',')[1])
-    #print 'nvar = ',nvar
+    if fileinput.lineno() == 1:
+      title,tvalue = line.split(',')
+      npuf = int(tvalue.split('=')[-1].split('"')[0])
+      #print 'npuf = ',npuf
 
-  if fileinput.lineno() == 3:
-    varNames = line.split(',')
-    for vNo, varName in enumerate(varNames):
-      varNames[vNo] = varName.replace('"','').upper()
-    #print varNames
-    
-    colnos   = []
-    for varName in nameList:
-      colnos.append(varNames.index(varName.upper()))
-    #print colnos 
-    
-    break
+    if fileinput.lineno() == 2:
+      nvar = int(line.split(',')[1])
+      #print 'nvar = ',nvar
 
-fileinput.close()
-#print 
+    if fileinput.lineno() == 3:
+      varNames = line.split(',')
+      for vNo, varName in enumerate(varNames):
+        varNames[vNo] = varName.replace('"','').upper()
+      #print varNames
+      
+      colNos = []
+      for varName in nameList:
+        colNo = varNames.index(varName.upper())
+        colNos.append(colNo)
+        #print colNo,varName
+      
+      #for colno in colNos:
+      #  sys.stdout.write('%14s'%varNames[colno])
+      #sys.stdout.write('\n')
 
-#for colno in colnos:
-#  sys.stdout.write('%14s'%varNames[colno])
-for varName in ['ipuf','x','y','z','c','sqrt(szz/sxx)','sqrt(szz/syy)']:
-  sys.stdout.write('%14s '%varName)
-sys.stdout.write('\n')
 
-pDat = []
+    if fileinput.lineno() < 4:
+      continue
 
-for line in fileinput.input(csvFile):
-  if fileinput.lineno() < 4:
-    continue
-  colVals = map(float,line.strip().split(',')[0:-1])
-  nameVals = {}
-  for varNo,colNo in enumerate(colnos):
-    nameVals.update({nameList[varNo]:colVals[colNo]})
+    colVals = map(float,line.strip().split(',')[0:-1])
 
-  colVals = [int(nameVals['ipuf']),nameVals['x'],nameVals['y'],nameVals['z'],nameVals['c'],np.sqrt(nameVals['szz']/nameVals['sxx']),np.sqrt(nameVals['szz']/nameVals['syy'])]
-  pDat.append(colVals) 
+    nameVals = {}
+    for varNo,colNo in enumerate(colNos):
+      nameVals.update({nameList[varNo]:colVals[colNo]})
+   
+    '''
+    if fileinput.lineno() == 4:
+      for varName in ['ipuf','x','y','z','c','sxz','szz','sxx','syz','syy']:
+        colNo = varNames.index(varName.upper())
+        sys.stdout.write('%7s(%2d) '%(varName,colNo))
+      sys.stdout.write('\n')
+    #colVals = [int(nameVals['ipuf']),nameVals['x'],nameVals['y'],nameVals['z'],nameVals['c'],\
+    #             nameVals['sxz']/np.sqrt(nameVals['szz']*nameVals['sxx']), nameVals['syz']/np.sqrt(nameVals['szz']*nameVals['syy'])]
+    '''
+
+    colVals  = []
+    # Variables names required for printing or plotting
+    varNames = ['ipuf','x','y','z','c','sxx','sxy','sxz','syy','syz','szz']
+    for varName in varNames 
+      if varName == 'ipuf':
+        colVals.append(int(nameVals[varName]))
+      else:
+        colVals.append(nameVals[varName])
+      if fileinput.lineno() == 4:
+        sys.stdout.write('%7s(%2d) '%(varName,colNo))
+    if fileinput.lineno() == 4:
+      sys.stdout.write('\n')
+      print colVals
+
+    pDat[fNo].append(tuple(colVals))
   
-  # ipuf 
-  #for colVal in colVals:
-  #  sys.stdout.write(' %13.5e '%colVal)
-  #sys.stdout.write('\n')
+    # ipuf 
+    maxLine = 100
+    if  fileinput.lineno() < maxLine:
+      for colVal in colVals:
+        sys.stdout.write(' %10.3e '%colVal)
+      sys.stdout.write('\n')
+    elif fileinput.lineno() == maxLine: 
+      sys.stdout.write('\n')
+      break
 
-  #if  fileinput.lineno() > 100:
-  #  break
+  fileinput.close()
+  print pDat[fNo][0],pDat[fNo][-1]
 
-fig = plt.figure(1)
-plt.clf()
-plt.hold(True)
-plt.setp(plt.gca(),frame_on=False,xticks=(),yticks=())
+if nFiles < 2:
+  p1 = np.array(pDat[0])
+  print p1.shape
 
-pDat = np.array(pDat)
-sub1 = fig.add_subplot(2,1,1)
-sub1.scatter(pDat[:,5],pDat[:,3]/1000.,marker='o',color='g')
-sub1.scatter(pDat[:,6],pDat[:,3]/1000.,marker='s',color='b')
-sub1.set_xlim([1e-4,1e2])
-sub1.set_xscale('log')
-sub1.set_xlabel('sz/sx')
-sub1.set_ylim([0,8])
-sub1.set_ylabel('Z')
+  p2 = np.array(pDat[1])
+  print p2.shape
 
-sub2 = fig.add_subplot(2,1,2)
-sub2.scatter(pDat[:,4],pDat[:,5],marker='o',color='g')
-sub2.scatter(pDat[:,4],pDat[:,6],marker='o',color='b')
-sub2.set_xlim([1e-6,1.])
-sub2.set_xscale('log')
-sub2.set_xlabel('C')
-sub2.set_ylim([1e-4,1e2])
-sub2.set_yscale('log')
-sub2.set_ylabel('sz/sx')
+  fig = plt.figure(1)
+  plt.clf()
+  plt.hold(True)
+  plt.setp(plt.gca(),frame_on=False,xticks=(),yticks=())
 
-plt.hold(False)
-figName = csvFile
-figName = figName.replace('.csv','.png')
-print figName
-plt.savefig(figName)
-fileinput.close()
+  sub1 = fig.add_subplot(2,1,1)
+  sub1.scatter(p1[:,4],p1[:,5],marker='+',color='g')
+  sub1.scatter(p2[:,4],p2[:,5],marker='x',color='b')
+  sub1.set_xlim([1e-6,1.])
+  sub1.set_xscale('log')
+  sub1.set_xlabel('C')
+  #sub1.set_ylabel('sxz/sqrt(sxx*szz) syz/sqrt(syy*szz)')
+  sub1.set_ylabel('sxz/sxx_szz')
+  sub1.set_yscale('log')
+  sub1.set_ylim([1e-4,1e2])
+
+  sub2 = fig.add_subplot(2,1,2)
+  sub2.scatter(p1[:,4],p1[:,6],marker='+',color='g')
+  sub2.scatter(p2[:,4],p2[:,6],marker='x',color='b')
+  sub2.set_xlim([1e-6,1.])
+  sub2.set_xscale('log')
+  sub2.set_xlabel('C')
+  sub2.set_ylabel('syz/syy_szz')
+  sub2.set_yscale('log')
+  sub2.set_ylim([1e-4,1e2])
+
+  plt.hold(False)
+  figName = csvFile[0].replace('.csv','') + csvFile[0].replace('.csv','') + '.png'
+  print figName
+  plt.savefig(figName)
