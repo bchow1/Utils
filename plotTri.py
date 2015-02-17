6162c776ed3aab038b18fac139911e1b779439b2
bnc <biswanath.chowdhury@sage-mgt.net>
2015-02-17 20:48:51 +0000

    ' Add plots for time breaks esp. for reverse projects'

--- plotTri.py
+++ plotTri.py
@@ -17,7 +17,7 @@ def printUsage():
   print 'Usage: plotTri -i inFile[.ntv] [-n t ] [-l skiplines ] [-s log/linear ]'
 
 #os.chdir('d:\\SCIPUFF\\runs\\EPRI\\IncrDose')
-os.chdir('d:\\SrcEst\\P1\\runs\\Outputs\\OnlySimple\\Simple\\ETEX')
+os.chdir('d:\\SrcEst\\P1\\runs\\Outputs\\OnlySimple\\ETEX\\FwdRev')
 
 arg = optparse.OptionParser()
 arg.add_option("-i",action="store",type="string",dest="inFile")
@@ -29,120 +29,125 @@ opt,args = arg.parse_args()
 
 print 'Current directory = ',os.getcwd()
 
-# Check arguments
-if opt.inFile:
-  inFile = opt.inFile
-else:
-  inFile = 'h42n40_v2_t0001.ntv'
-  #print 'Error: inFile must be specified'
-  #printUsage()
-  #sys.exit()
-    
-if opt.nSkip:
-  nSkip = opt.nSkip
+for tbreak in range(10):
 
-if opt.isLatLon[0].lower() == 't':
-  isLatLon = True
-else:
-  isLatLon = False
-
-if opt.isLog.lower().strip() == 'log':
-  isLog = True
-else:
-  isLog = False
+  inFile = 'RevEtex_t%04d.ntv'%(tbreak+1)
+  print inFile
+  '''
+  # Check arguments
+  if opt.inFile:
+    inFile = opt.inFile
+  else:
+    inFile = 'RevEtex_t%4d.ntv'%tbreak
+    #print 'Error: inFile must be specified'
+    #printUsage()
+    #sys.exit()
+  '''
+  if opt.nSkip:
+    nSkip = opt.nSkip
   
-if inFile.endswith('.ntv'):
-  inFile = inFile.replace('.ntv','')
-
-if not os.path.exists(inFile + '.ntv'):
-  print 'Error: cannot find file %s.ntv'%inFile
-  sys.exit()
-
-if not os.path.exists(inFile + '.tri'):
-  print 'Error: cannot find file %s.tri'%inFile
-  sys.exit()
-
-nodeData = np.loadtxt(inFile+'.ntv', skiprows=nSkip, usecols=(0, 1, 2, 5), dtype={'names':('nId','x','y','cmean'),\
-                      'formats':('int','float','float','float')})
-triData  = np.loadtxt(inFile+'.tri', skiprows=1, usecols=(0, 1, 2, 3), dtype={'names':('tId','nIda','nIdb','nIdc'),\
-                      'formats':('int','int','int','int')})
-
-nodeData = np.sort(nodeData,order='nId')
-triData  = np.sort(triData,order='tId')
-
-triangles = np.array([triData['nIda']-1,triData['nIdb']-1,triData['nIdc']-1])
-
-# Rotate for LatLon
-if isLatLon:
-  y = nodeData['x']
-  x = nodeData['y']
-else:
-  x = nodeData['x']
-  y = nodeData['y']
-c = nodeData['cmean']
-
-npts = len(x)
-maxc = max(c)
-
-print 'Max Value = ',maxc
-print 'No. of points = ',npts
-
-c = c/maxc
-c = ma.masked_where(c<1e-30,c)
-c = ma.filled(c,1e-30)
-
-if isLog:
-  logBase = 10.
-  clrmin = -6
-  clrmax = 0
-  clrlev = (clrmax - clrmin + 1)
-  levels = np.logspace(clrmin,clrmax,num=clrlev,base=logBase)
-  clrmap = cm.get_cmap('jet',clrlev-1)
-  lnorm  = colors.LogNorm(levels,clip=False)
-else:
-  clrmin = 0.
-  clrmax = 1.
-  clrlev = 11 
-  levels = np.linspace(clrmin,clrmax,num=clrlev)
-  clrmap = cm.get_cmap('jet',clrlev-1)
-  lnorm  = colors.Normalize(levels,clip=False)
-
-print levels
-
-#outFile = open(inFile+'_out.dat','w',0)
-#for i in range(len(c)):
-#  if c[i] > 1e-18:
-#    outFile.write('%g %g %g\n'%(x[i],y[i],c[i]))
-#outFile.close()
-
-# tricontour.
-fig = plt.figure()
-plt.clf()
-plt.hold(True)
-if isLog:
-  cax = plt.tricontourf(x,y,c, triangles=triangles, norm= lnorm, levels = levels, cmap=plt.cm.jet, vmin = 10**clrmin)
-else:
-  cax = plt.tricontourf(x,y,c, triangles=triangles, norm= lnorm, levels = levels, cmap=plt.cm.jet, vmin=0.01,extend='both')
-cax.cmap.set_under('white')
-
-if isLog:
-  cax.set_clim(10**clrmin,10**clrmax)
-  cbar = plt.colorbar(ticks=levels,format="%5.3e")
-  cbar.ax.set_yticklabels(levels)
-else:
-  cax.set_clim(0.01,1.01)
-  cbar = plt.colorbar(ticks=levels,format="%3.1f")
-  cbar.ax.set_yticklabels(levels-0.01)
-plt.tricontour(x,y,c, triangles=triangles, norm= lnorm, levels = levels, colors='k')
-
-if isLatLon:
-  plt.xlabel('Longitude')
-  plt.ylabel('Latitude')
-else:
-  plt.xlabel('X')
-  plt.ylabel('Y')
-plt.hold(False)
-plt.title('Concentrations for '+ inFile + ' scaled with %8.3e'%maxc)
-fig.savefig(inFile+'.png')
-#plt.show()
+  if opt.isLatLon[0].lower() == 't':
+    isLatLon = True
+  else:
+    isLatLon = False
+  
+  if opt.isLog.lower().strip() == 'log':
+    isLog = True
+  else:
+    isLog = False
+    
+  if inFile.endswith('.ntv'):
+    inFile = inFile.replace('.ntv','')
+  
+  if not os.path.exists(inFile + '.ntv'):
+    print 'Error: cannot find file %s.ntv'%inFile
+    sys.exit()
+  
+  if not os.path.exists(inFile + '.tri'):
+    print 'Error: cannot find file %s.tri'%inFile
+    sys.exit()
+  
+  nodeData = np.loadtxt(inFile+'.ntv', skiprows=nSkip, usecols=(0, 1, 2, 5), dtype={'names':('nId','x','y','cmean'),\
+                        'formats':('int','float','float','float')})
+  triData  = np.loadtxt(inFile+'.tri', skiprows=1, usecols=(0, 1, 2, 3), dtype={'names':('tId','nIda','nIdb','nIdc'),\
+                        'formats':('int','int','int','int')})
+  
+  nodeData = np.sort(nodeData,order='nId')
+  triData  = np.sort(triData,order='tId')
+  
+  triangles = np.array([triData['nIda']-1,triData['nIdb']-1,triData['nIdc']-1])
+  
+  # Rotate for LatLon
+  if isLatLon:
+    y = nodeData['x']
+    x = nodeData['y']
+  else:
+    x = nodeData['x']
+    y = nodeData['y']
+  c = nodeData['cmean']
+  
+  npts = len(x)
+  maxc = max(c)
+  
+  print 'Max Value = ',maxc
+  print 'No. of points = ',npts
+  
+  c = c/maxc
+  c = ma.masked_where(c<1e-30,c)
+  c = ma.filled(c,1e-30)
+  
+  if isLog:
+    logBase = 10.
+    clrmin = -6
+    clrmax = 0
+    clrlev = (clrmax - clrmin + 1)
+    levels = np.logspace(clrmin,clrmax,num=clrlev,base=logBase)
+    clrmap = cm.get_cmap('jet',clrlev-1)
+    lnorm  = colors.LogNorm(levels,clip=False)
+  else:
+    clrmin = 0.
+    clrmax = 1.
+    clrlev = 11 
+    levels = np.linspace(clrmin,clrmax,num=clrlev)
+    clrmap = cm.get_cmap('jet',clrlev-1)
+    lnorm  = colors.Normalize(levels,clip=False)
+  
+  print levels
+  
+  #outFile = open(inFile+'_out.dat','w',0)
+  #for i in range(len(c)):
+  #  if c[i] > 1e-18:
+  #    outFile.write('%g %g %g\n'%(x[i],y[i],c[i]))
+  #outFile.close()
+  
+  # tricontour.
+  fig = plt.figure()
+  plt.clf()
+  plt.hold(True)
+  if isLog:
+    cax = plt.tricontourf(x,y,c, triangles=triangles, norm= lnorm, levels = levels, cmap=plt.cm.jet, vmin = 10**clrmin)
+  else:
+    cax = plt.tricontourf(x,y,c, triangles=triangles, norm= lnorm, levels = levels, cmap=plt.cm.jet, vmin=0.01,extend='both')
+  cax.cmap.set_under('white')
+  
+  if isLog:
+    cax.set_clim(10**clrmin,10**clrmax)
+    cbar = plt.colorbar(ticks=levels,format="%5.3e")
+    cbar.ax.set_yticklabels(levels)
+  else:
+    cax.set_clim(0.01,1.01)
+    cbar = plt.colorbar(ticks=levels,format="%3.1f")
+    cbar.ax.set_yticklabels(levels-0.01)
+  plt.tricontour(x,y,c, triangles=triangles, norm= lnorm, levels = levels, colors='k')
+  
+  if isLatLon:
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+  else:
+    plt.xlabel('X')
+    plt.ylabel('Y')
+  plt.hold(False)
+  plt.title('Concentrations for '+ inFile + ' scaled with %8.3e'%maxc)
+  fig.savefig(inFile+'.png')
+  #plt.show()
 
