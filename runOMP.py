0551f8bdfdd9fe999911acef2ed5c5eb35d39fbf
bnc <biswanath.chowdhury@sage-mgt.net>
2015-05-19 16:51:16 -0400

    Add counting of srf cells

--- runOMP.py
+++ runOMP.py
@@ -5,6 +5,7 @@ import shutil
 import socket
 import subprocess
 import fileinput
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -19,27 +20,29 @@ if compName == 'sm-bnc' or compName == 'sage-d600':
 if compName == 'pj-linux4':
   sys.path.append('/home/user/bnc/python')
   
+if os.sys.platform == 'win32':
+  binDir      = "D:\\SCIPUFF\\CVS_REPO\\workspace\\DTRA\\vs2008_Reverse\\bin\\intel\\Win32\\Debug"
+  vendir      = "D:\\SCIPUFF\\CVS_REPO\\bin\\vendor\\Win32" 
+  iniFile     = "D:\\SCIPUFF\\CVS_REPO\\workspace\\DTRA\\vs2008\\scipuff.ini"
+  runDir      = 'D:\\SrcEst\\P1\\runs\\Outputs\\OnlySimple\\Simple\\simplei'
+  env["PATH"] = "%s;%s;%s" % (binDir,vendir,env["PATH"])
+  runsci      = os.path.join(binDir,'runsci.exe')
+  rdsrf       = os.path.join(binDir,'readsrf.exe')
+else:
+  binDir      = "/home/user/bnc/scipuff/Repository/UNIX/AFTACx64_OMP/bin/linux/ifort"
+  iniFile     = "scipuff.ini"
+  runDir      = '/home/user/bnc/scipuff/runs/AFTAC/OpenMP/StLouis_Jan06'
+  env["PATH"] = "%s:%s" % (binDir,env["PATH"])
+  runsci      = os.path.join(binDir,'runsci')
+  rdsrf       = os.path.join(binDir,'readsrf')
+
+os.chdir(runDir)
+print 'Using runsci from ',binDir,' in ',runDir,'\n'
+tSecs = os.path.getmtime(os.path.join(binDir,runsci))
+print '  with mod time of %s'%datetime.datetime.fromtimestamp(tSecs)
+print env["PATH"],'\n'  
+  
 def runPrj(basePrj,ntList):
-
-  if os.sys.platform == 'win32':
-    binDir      = "D:\\SCIPUFF\\CVS_REPO\\workspace\\DTRA\\vs2008_Reverse\\bin\\intel\\Win32\\Debug"
-    vendir      = "D:\\SCIPUFF\\CVS_REPO\\bin\\vendor\\Win32" 
-    iniFile     = "D:\\SCIPUFF\\CVS_REPO\\workspace\\DTRA\\vs2008\\scipuff.ini"
-    runDir      = 'D:\\SrcEst\\P1\\runs\\Outputs\\OnlySimple\\Simple\\simplei'
-    env["PATH"] = "%s;%s;%s" % (binDir,vendir,env["PATH"])
-    runsci       = os.path.join(binDir,'runsci.exe')
-  else:
-    binDir      = "/home/user/bnc/scipuff/Repository/UNIX/AFTACx64_OMP/bin/linux/ifort"
-    iniFile     = "scipuff.ini"
-    runDir      = '/home/user/bnc/scipuff/runs/AFTAC/OpenMP/stLouis2'
-    env["PATH"] = "%s:%s" % (binDir,env["PATH"])
-    runsci       = os.path.join(binDir,'runsci')
-    runsci      = os.path.join(binDir,'runsci')
-
-  os.chdir(runDir)
-  print 'Using runsci from ',binDir,' in ',runDir,'\n'
-  print env["PATH"],'\n'
-
   
   prjNames = []
   sfxList = ['inp','scn','msc']
@@ -52,22 +55,25 @@ def runPrj(basePrj,ntList):
     shutil.copy(iniFile,'scipuff.ini')
     runSCI(env,runsci,prjName,iniFile)
 
-def crtNpArray(prjNames):
+def crtNpArray(prjNames,countCell=False):
+  
   for prjNo,prjName in enumerate(prjNames):
-    nPuffs = countPuffs(prjName)
-    print prjName,np.shape(nPuffs)
+    if countCell:
+      nPuffs = countNcells(prjName)
+    else:
+      nPuffs = countPuffs(prjName)
+    print 'prjName, Shape(nPuffs) = ',prjName,np.shape(nPuffs)
     if prjName == prjNames[0]:
       nt = len(nPuffs)
-      print nt,len(prjNames)+1
       npArray = np.zeros((nt,len(prjNames)+1))
       npArray[:,0] = nPuffs[:,0]
       npArray[:,1] = nPuffs[:,1]
     else:
-      nt = min(nt,np.shape(nPuffs)[0])
-      npArray[:nt,prjNo+1] = nPuffs[:nt,1] -  npArray[:nt,1]
-  return npArray
+      nt = min(nt,np.shape(nPuffs)[0]) 
+      npArray[:nt,prjNo+1] = nPuffs[:nt,1] -  npArray[:nt,1]  
   prtNPuff(prjNames,npArray)
-  
+  return npArray
+
 def prtNPuff(prjNames,npArray):
   sys.stdout.write('Hr ')
   for prj in prjNames:
@@ -128,6 +134,64 @@ def countPuffs(prjName):
   nPuffs = np.array(nPuffs)       
   return nPuffs
 
+def crtNcArray(env,readsrf,prjNames):
+  
+  for sfx in ['dos','dep']: 
+  
+    outFiles = []
+    for prjNo,prjName in enumerate(prjNames):
+          
+      print 'Running readsrf for %s.%s '%(prjName,sfx)
+      
+      outFile = '%s_%s.output'%(prjName,sfx)
+      #if os.path.exists(outFile):
+      #  os.remove(outFile)
+      
+      if not os.path.exists(outFile):
+        rdsrf_inp = open('rdsrf.input','w') 
+        rdsrf_inp.write('%s.%s \nb \n -1\n'%(prjName,sfx))
+        rdsrf_inp.close()
+      
+        rdsrf_inp = open('rdsrf.input','r')
+        rdsrf_out = open(outFile,'w')
+        rdsrf_err = open('rdsrf.error','w')
+        h = subprocess.Popen(rdsrf, env=env, bufsize=0, shell=False,stdin=rdsrf_inp, stdout=rdsrf_out,stderr=rdsrf_err)
+        h.communicate()
+        rdsrf_inp.close()
+        rdsrf_out.close()
+        rdsrf_err.close()
+      
+      #ncells = countNcells(outFile)
+      outFiles.append(outFile)          
+    crtNpArray(outFiles,countCell=True) 
+     
+  return
+
+def countNcells(outFile):
+
+  nCells   = []
+  rdNcells = False
+  
+  if os.path.exists(outFile):
+    for line in fileinput.input(outFile):
+      if '======' in line:
+        rdNcells = True
+        continue
+      if rdNcells:
+        lvars = line.strip().split()
+        if len(lvars) > 0:
+          nCells.append(map(float,(lvars[1],lvars[2])))
+        else:
+          break
+      else:
+        continue
+    fileinput.close()
+  else:
+    print 'Missing :',outFile
+     
+  nCells = np.array(nCells)       
+  return nCells
+
 def pltnPuffs(nPuffs,ip,ntList):
   
   global Lh,Lk
@@ -158,11 +222,22 @@ def pltnPuffs(nPuffs,ip,ntList):
   return
 
 if __name__ == '__main__':
+  
   ntList = [1,4]
   basePrj = 'stLouis'
   runPrj(basePrj,ntList) 
-  prjNames  = ['stLouis_p1','stLouis_p4','win_p1','win_p4','sL08_t1','sL08_t4']
+  
+  # In /home/user/bnc/scipuff/runs/AFTAC/OpenMP/stLouis2
+  # prjNames  = ['stLouis_p1','stLouis_p4','win_p1','win_p4','sL08_t1','sL08_t4']
+  
+  # In /home/user/bnc/scipuff/runs/AFTAC/OpenMP/StLouis_Jan06'
+  prjNames  = ['stLouis_p1','stLouis_p4' ] #,'win_p1','win_p4']
+  
   npArray = crtNpArray(prjNames)
-  prtNPuff(prjNames,npArray)
+  #prtNPuff(prjNames,npArray)
+  
+  crtNcArray(env,rdsrf,prjNames)
+  #prtNCells(prjNames,ncCells)
+  
   #nPuffs = countPuffs(prjName)
   #pltnPuffs(nPuffs,iproc,ntList) 
