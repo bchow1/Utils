a0f754ef157e4a831a2e2527f50a98751a8360c6
bnc <biswanath.chowdhury@sage-mgt.net>
2015-06-23 02:22:16 +0100

    'Plot each arc separately'

--- plotCop.py
+++ plotCop.py
@@ -33,12 +33,15 @@ def readcOP(fileName, run):
       cOP.append(map(float,arc3))
   cOP = getMaskedArray(cOP)
   arcOP = np.array(arcOP)
-  #print arcOP.shape
+  print arcOP.shape
+  print run
+  
+  plt.hold(True)
   for arc in range(3):
     getStats(arcOP[arc,:,1], arcOP[arc,:,0], statFile, run ,str(arc))
   #print cOP  #
 
-  return (cOP)
+  return (cOP,arcOP)
 
 def getMaskedArray(cOP):
     cOP = np.array(cOP)
@@ -89,7 +92,7 @@ def getStats(data1, data2, statFile, case, arc):
         print i,diff1,diff2,diff3
         plt.scatter(diff2,diff1,color='red')
         plt.scatter(diff2,diff3,color='green')
-        plt.show()
+        #plt.show()
       #plt.hold(False)
       #plt.show() 
           
@@ -179,17 +182,22 @@ statFile = open("COP_stat.csv","w")
 statFile.write("Case, Arc, upa, nmse_1, mfbe, fac2\n")
 
 fileName = os.path.join(runDir,'cop.all')
-cSkewOP  = readcOP(fileName, "Skew")
-print 'cSkewOP',cSkewOP
+cSkewOP,arcSkewOP  = readcOP(fileName, "Skew")
+#print 'cSkewOP',cSkewOP
+#print 'arcSkewOP',arcSkewOP
 
 fileName = os.path.join(runDir,'SCICHEM-01','cop.all')
-cStdOP   = readcOP(fileName, "Standard")
+cStdOP,arcStdOP   = readcOP(fileName, "Standard")
+'''
 print 'cStdOP',cStdOP
+print 'arcStdOP',arcStdOP
+
 fig = plt.figure()
 plt.clf()
 fig.subplots_adjust(bottom=0.2)
 ax = fig.add_subplot(111)
 plt.hold(True)
+
 hskew = plt.scatter(cSkewOP[:,0],cSkewOP[:,1],color='black',marker='o',s=30)
 hstd  = plt.scatter(cStdOP[:,0],cStdOP[:,1],color='black',marker='^',s=30)
 
@@ -206,9 +214,55 @@ plt.xlabel(r'Observed ($\mu g/m^3$)', fontsize=10)
 plt.ylabel(r'Predicted ($\mu g/m^3$)', fontsize = 10)
 figCaption = 'Figure 2:  Quantile-Quantile plot of concentration for Copenhagen data'
 plt.text(-0.01,-1.3,figCaption,fontsize=10)
-plt.legend([hskew,hstd],['Skew Model','Standard Model'],bbox_to_anchor=(0.25,0.97))
+plt.legend([hskew,hstd],['Skewed Model','Standard Model'],bbox_to_anchor=(0.25,0.97))
 plt.hold(False)
 #plt.show()
 plt.savefig('cop_ord.eps', dpi=300)
+print 'Done plotting in ',os.getcwd()
+'''
+#clr = ['1.0','0.75','0.5']
+#clr = ['red','blue','green']
+cMx = [[.5,8.],[0.,4.],[0.,3.]]
+
+for arc in range(3):
+  
+  stdCo = arcStdOP[arc,:,0]
+  stdCp = arcStdOP[arc,:,1]
+  stdCp = np.sort(stdCp[stdCo > 0.])*1e-3
+  stdCo = np.sort(stdCo[stdCo > 0.])*1e-3
+  
+  skewCo = arcSkewOP[arc,:,0]
+  skewCp = arcSkewOP[arc,:,1]
+  skewCp = np.sort(skewCp[skewCo > 0.])*1e-3
+  skewCo = np.sort(skewCo[skewCo > 0.])*1e-3
+  
+  print 'Arc = ',arc,skewCo[0],skewCp[0],stdCo[0],stdCp[0]
+  #print 'std'
+  #for i in range(len(stdCo)):
+  #  print i,stdCo[i],stdCp[i]
+  #print 'skew'
+  #for i in range(len(skewCp)):
+  #  print i,skewCo[i],skewCp[i]
+  
+  fig = plt.figure()
+  plt.clf()
+  plt.hold(True)
+  
+  hskew = plt.scatter(skewCo,skewCp,color='k',marker='o',s=30)
+  hstd  = plt.scatter(stdCo,stdCp,color='k',marker='^',s=30)
+  
+  vmin = cMx[arc][0]
+  vmax = cMx[arc][1]
+  
+  plt.xlim([vmin,vmax])
+  plt.ylim([vmin,vmax])
+  plt.plot([vmin,vmax],[vmin,vmax],'k-')
+  plt.xlabel(r'Observed ($\mu g/m^3$)', fontsize=10)
+  plt.ylabel(r'Predicted ($\mu g/m^3$)', fontsize = 10)
+  plt.legend([hskew,hstd],['Skewed Model','Standard Model'],bbox_to_anchor=(0.25,0.97))
+  
+  plt.hold(False)
+  plt.savefig('cop_ord_arc%d.png'%arc)
+  
 print 'Done plotting in ',os.getcwd()
 
